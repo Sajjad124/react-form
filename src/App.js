f08700eb55d3f143ef270932cfa1a5897a8016d5
Muhammad Umair <181370124@gift.edu.pk>
2022-07-30 23:28:30 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
 import "./App.css";
-import img from "./download.png";
 
 function App() {
   return (
     <div className="div-main">
+
       <div className="inner">
         <h1>Hello</h1>
         <input type="text" placeholder="Enter input"></input>
