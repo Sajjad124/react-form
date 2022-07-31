8fdd44bb32fe15b466d5d5dec5cf77a4970daeff
Muhammad Umair <181370124@gift.edu.pk>
2022-07-31 21:32:15 +0500

    updated onclick
     Shown the input value using hooks and event onChange and onClick in button also by going through the controlled and uncontrolled components

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const[name,setName] = useState();
+  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState();
 
-  const inputEvent = (event) =>{
-  console.log(event.target.value)
-  setName(event.target.value)
-}
+  const onSubmit = () =>{
+    setFullName(name)
+  }
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    setName(event.target.value);
+  };
   return (
     <div className="div-main">
       <div className="inner">
-        <h1>Hello {name}</h1>
-        <input type="text" placeholder="Enter input" onChange={inputEvent} />
-        <button>Click Me 👍</button>
+        <h1>Hello {fullName}</h1>
+        <input type="text" placeholder="Enter input" value={name} onChange={inputEvent} />
+        <button onClick={onSubmit} >Click Me 👍</button>
       </div>
     </div>
   );
