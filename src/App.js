94743eca2c5609ff3274236750d71d557483e4b2
Muhammad Umair <181370124@gift.edu.pk>
2022-07-31 21:00:57 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 
 function App() {
+const inputEvent = () =>{
+  console.log("clicked")
+}
   return (
     <div className="div-main">
-
       <div className="inner">
         <h1>Hello</h1>
-        <input type="text" placeholder="Enter input"></input>
+        <input type="text" placeholder="Enter input" onChange={inputEvent} />
         <button>Click Me 👍</button>
       </div>
     </div>
