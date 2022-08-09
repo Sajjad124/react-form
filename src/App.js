9c18bae87e76212f3c26f47e7c134d13fbeab9fb
Muhammad Umair <181370124@gift.edu.pk>
2022-08-10 00:34:36 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,42 +1,52 @@
 import { useState } from "react";
 import "./App.css";
 function App() {
-  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lName: "",
+  });
 
-  const [fullName, setFullName] = useState();
-  const [lastname, setLastName] = useState("");
+  // remove these states and use only one state
+  // const [fullName, setFullName] = useState();
+  // const [lastname, setLastName] = useState("");
+
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    setFullName(event.target.value);
+  };
 
   const onSubmits = (event) => {
     event.preventDefault();
-    setFullName(name);
-    setLastName(lastname);
+    alert("Form Submitted");
+    // setFullName(name);
+    // setLastName(lastname);
+  };
 
-  };
-  const inputEvent = (event) => {
-    console.log(event.target.value);
-    setName(event.target.value);
-  };
-  const inputEventTwo = (event) => {
-    console.log(event.target.value);
-    setLastName(event.target.value);
-  };
   return (
     <div className="div-main">
       <form onSubmit={onSubmits}>
         <div className="inner">
-          <h1>Hello {fullName +" "+ lastname}</h1>
+          <h1>Hello{fullName.fName + +fullName.lName} </h1>
           <input
             type="text"
             placeholder="Enter input"
-            value={name}
+            name="fName"
+            value={fullName.fName}
             onChange={inputEvent}
           />
           <input
             type="text"
             placeholder="Enter your last name"
+            name="lName"
+            onChange={inputEvent}
+            value={fullName.lName}
+          />
+          {/* <input
+            type="text"
+            placeholder="Enter password"
             onChange={inputEventTwo}
             value={lastname}
-          />
+          /> */}
           <button type="submit">Click Me 👍</button>
         </div>
       </form>
